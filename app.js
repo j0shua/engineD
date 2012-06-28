ac47477d4763e72a0077dabee0a147f9adb94968
Josh Goldsmith <jgoldsmith@businessinsider.com>
2012-06-28 15:38:07 -0400

    added some routes for comment/add and comment/list/:ts

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
-
 var app = require('express').createServer();
+
+var comments = [];
+
 
 app.configure('development', function(){
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.bodyParser());
 });
 app.listen(3000);
 
@@ -14,6 +17,25 @@ app.get('/', function(req, res){
   res.send('hi');
 });
 
-app.get('/hi', function(req, res){
-  res.send('hi');
+app.post('comment/add', function(req, res){
+  comments.push(req.body);
+  res.send({200: 'ok');
+});
+
+app.get('/comment/list/:ts?', function(req, res){
+  var ts = req.params.ts;
+  if (!ts){
+    res.send(comments);
+  } else {
+    var current;
+    for (var i=0; i<comments.length; i+=1){
+      current = comments[i];
+      if (current.ts < ts){
+        continue;
+      }
+      break;
+    }
+    var subset = comments.slice(i);
+    res.send(subset);
+  }
 });
