285bb9d7b6898a58cf538cfe9a134d3f01cc354f
Josh Goldsmith <jgoldsmith@businessinsider.com>
2012-06-28 16:33:30 -0400

    added rooms as a higher level to comments

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = require('express').createServer();
 
-var comments = [];
-
-
+// ==section== app configurations
 app.configure('development', function(){
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -11,23 +9,97 @@ app.configure('development', function(){
 });
 app.listen(3000);
 
+// ==section== vars
+var rooms = [
+    {
+      id: 'roomOne',
+      comments: []
+    }, 
+    {
+      id: 'roomTwo',
+      comments: []
+    }, 
+    {
+      id: 'roomThree',
+      comments: []
+    }, 
+];
+
+// ==section==  helper functions
+// check for existence of a room; if found return it's index
+function roomIndex(id){
+  for (var i=0; i<rooms.length; i+=1){
+    if (rooms[i].id == id){
+      return i;
+    }
+  }
+}
+
+// find a room in the array; if found return it
+function findRoom(id){
+  for (var i=0; i<rooms.length; i+=1){
+    if (rooms[i].id == id){
+      return rooms[i];
+    }
+  }
+  return false;
+}
+
+// ==section== routing
+
 // lets create some vars for the data
 // break out routes into a route file ?
 app.get('/', function(req, res){
   res.send('hi');
 });
 
+// list rooms that exist
+// returns an array of room_ids formatted like this:
+// [
+//    {id: room_id},
+//    {id: room_id},
+//    {id: room_id}
+// ]
+app.get('/room/list', function(req, res){
+  var list = [];
+  for (var i=0; i<rooms.length; i+=1){
+    list.push({id: rooms[i].id});
+  }
+  res.json(list);
+});
+
+// todo add check for no room id
+// add a comment to a room
 app.post('/comment/add', function(req, res){
-  comments.push(req.body);
+  var room_id = req.body.room_id;
+  if (!room_id) { 
+    res.send(404);
+  }
+
+  var room = findRoom(room_id);
+  if (!room){
+    res.send(404);
+  }
+  var room_index = roomIndex(room_id);
+  rooms[room_index].comments.push(req.body);
   res.send({200: 'ok'});
 });
 
 app.get('/comment/list/:ts?', function(req, res){
   var ts = req.params.ts;
-  if (!ts){
-    res.send(comments);
-  } else {
-    var current;
+  var room_id = req.body.room_id;
+  if (!room_id) { 
+    res.send(404);
+  }
+
+  var room = findRoom(room_id);
+  if (!room){
+    res.send(404);
+  }
+
+  var current;
+  var comments = room.comments;
+  if (ts){
     for (var i=0; i<comments.length; i+=1){
       current = comments[i];
       if (current.ts < ts){
@@ -35,7 +107,8 @@ app.get('/comment/list/:ts?', function(req, res){
       }
       break;
     }
-    var subset = comments.slice(i);
-    res.send(subset);
+    comments = comments.slice(i);
   }
+    
+  res.send(comments);
 });
